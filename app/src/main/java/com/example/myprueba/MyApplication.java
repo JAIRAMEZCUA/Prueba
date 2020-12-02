d254ad14402fefd44396a9c125f686a44db558b0
ENRIQUE AMEZCUA <jamezcua@na-at.com.mx>
2020-12-01 21:54:28 -0600

    CambiosAlCIclor

---
+++ app/src/main/java/com/example/myprueba/MyApplication.java
@@ -0,0 +1,12 @@
+package com.example.myprueba;
+
+import android.app.Application;
+import android.util.Log;
+
+public class MyApplication  extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("TAG","OnCreate");
+    }
+}
